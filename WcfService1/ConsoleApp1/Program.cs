9d13671c3c7ab50a34541c489869c6393a7ad0eb
StoneIsDeveloper <m18146611430@163.com>
2018-09-13 23:10:57 +0800

    add wcf demo

---
+++ WcfService1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using MagicEightBallServiceLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost  serviceHost = new ServiceHost(typeof(MagicEightBallService) ))
+            {
+                serviceHost.Open();
+                foreach (System.ServiceModel.Description.ServiceEndpoint se in serviceHost.Description.Endpoints)
+                {
+                    Console.WriteLine("Address:{0}", se.Address);
+                    Console.WriteLine("Binding.Name:{0}", se.Binding.Name);
+                    Console.WriteLine("Binding.Name:{0}", se.Binding.Name);
+
+                }
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
---
+++ WcfService1/MagicEightBallServiceLib/MagicEightBallService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService : IEightBall
+    {
+        public MagicEightBallService()
+        {
+            Console.WriteLine("enter question...");
+        }
+
+        public string ObtianAnswerToQuestion(string question)
+        {
+            return $"no anser for: {question}";
+        }
+    }
+}
